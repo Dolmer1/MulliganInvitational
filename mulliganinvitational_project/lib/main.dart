359b11b433c4f90c477ebed04c8b933199b7c403
Dolmer1 <72791404+Dolmer1@users.noreply.github.com>
2022-11-15 21:24:06 +0100

    .

--- mulliganinvitational_project/lib/main.dart
+++ mulliganinvitational_project/lib/main.dart
@@ -10,12 +10,14 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text('Testing'),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 25, 63, 27),
+              title: const Text('Mulligan Invitational'),
+            ),
+            body: Container(
+                child: const Text('Testing'),
+                margin: const EdgeInsets.all(50),
+                padding: const EdgeInsets.all(50))));
   }
 }
