ef769cf7bc7c363973677f4277f6462314ced00b
Dolmer1 <72791404+Dolmer1@users.noreply.github.com>
2022-11-17 21:37:31 +0100

    1

--- mulliganinvitational_project/lib/main.dart
+++ mulliganinvitational_project/lib/main.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyWidget());
-}
+void main() => runApp(const MyApp());
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Color.fromARGB(255, 25, 63, 27),
-              title: const Text('Mulligan Invitational'),
-            ),
-            body: Container(
-                child: const Text('Testing'),
-                margin: const EdgeInsets.all(50),
-                padding: const EdgeInsets.all(50))));
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.safety_divider),
+      ),
+    );
   }
 }
